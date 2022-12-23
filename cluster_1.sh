de34a75fa893de8208da190b654c8350a712eab0
Itay Haike <81685066+itayhaike@users.noreply.github.com>
2022-12-23 15:49:44 +0200

    Update cluster_1.sh

--- cluster_1.sh
+++ cluster_1.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Important!!!!!!"
 sleep 1
-echo "!!!Make sure you edit the file: 'sudo visudo' by this template: $(whoami) ALL=(ALL) NOPASSWD:ALL !!!"
-sleep 2
+echo "!!!Make sure you edit the file: 'sudo visudo' by this template: $(whoami) ALL=(ALL) NOPASSWD:ALL !!!\n and group:"
+' %sudo  ALL=(ALL:ALL) NOPASSWD:ALL '
+sleep 4
 sudo nano /etc/sudoers
 sleep 2
 
